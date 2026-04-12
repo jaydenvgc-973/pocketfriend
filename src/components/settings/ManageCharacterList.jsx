3124ab6f52f80e5161ff9ab8070d1c9e1cc8e70d
Jayden Jackson <murqart@gmail.com>
2026-04-12 04:35:13 +0000

    File changes

--- src/components/settings/ManageCharacterList.jsx
+++ src/components/settings/ManageCharacterList.jsx
@@ -23,7 +23,7 @@ export default function ManageCharacterList() {
   const { data: characters = [] } = useQuery({
     queryKey: ['characters', currentUser?.email],
     queryFn: () => currentUser?.email
-      ? base44.entities.Character.filter({ created_by: currentUser.email }, '-created_date')
+      ? base44.entities.Character.filter({ created_by: currentUser.email }, '-created_date', 200)
       : [],
     enabled: !!currentUser?.email,
   });
