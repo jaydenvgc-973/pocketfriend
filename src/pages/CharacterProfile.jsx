f605b79d965e77758f09ffc208fe95df0c54eca9
Jayden Jackson <murqart@gmail.com>
2026-04-19 15:17:32 +0000

    File changes

--- src/pages/CharacterProfile.jsx
+++ src/pages/CharacterProfile.jsx
@@ -390,7 +390,7 @@ export default function CharacterProfile() {
                 </div>
               </div>
             ))}
-            <CharacterFeelingsCard character={character} />
+            <CharacterFeelingsCard character={character} onRespectCorrected={refetch} />
           </div>
         </div>
 
